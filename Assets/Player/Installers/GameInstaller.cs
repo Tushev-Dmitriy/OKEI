7510d6ec7383b2b40986f1b60716becb06bbb0ba
Tushev-Dmitriy <tushev-dmitriy@mail.ru>
2025-09-25 14:06:14 +0500

    Add Bitgem StylisedWater URP asset package
    Imported Bitgem StylisedWater URP package including example scene, materials, meshes, scripts, shaders, and textures. Added supporting meta files and updated project assets and settings to integrate the new water system.

--- Assets/Player/Installers/GameInstaller.cs
+++ Assets/Player/Installers/GameInstaller.cs
@@ -6,9 +6,10 @@ using Zenject;
 public class GameInstaller : MonoInstaller
 {
     [SerializeField] private GameObject _playerPrefab;
-
+    [SerializeField] private Vector3 _playerPos;
     public override void InstallBindings()
     {
+        _playerPrefab.transform.position = _playerPos;
         Container.Bind<ThirdPersonController>()
             .FromComponentInNewPrefab(_playerPrefab)
             .AsSingle()
